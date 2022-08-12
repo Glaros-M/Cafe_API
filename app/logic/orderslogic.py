554ff171f4f50c15198a0ac1385ab14dd0877734
glaros-m <glaros-m@yandex.ru>
2022-08-12 12:42:34 +0300

    Добавил логику. Нужен небольшой рефракторинг кода функции

--- app/logic/orderslogic.py
+++ app/logic/orderslogic.py
@@ -7,12 +7,20 @@ from fastapi import HTTPException
 from ..db import crud
 
 
+def _check_order_and_recipe(item_to_create: schemas.OrderItemsCreate2, db: Session):
+    order: schemas.Orders = crud.OrdersCRUD.read_one(item_to_create.OrderId, db)
+    recipe: schemas.Recipe = db.get(models.Recipes, item_to_create.RecipeId)
+    if not order:
+        raise HTTPException(status_code=404, detail=f"Order not found")
+    if not recipe:
+        raise HTTPException(status_code=404, detail=f"Recipe not found")
+
+
 def add_item_to_order(item: schemas.OrderItemsCreate, order_id: int, db: Session):
     item_to_create = schemas.OrderItemsCreate2(**item.dict(), OrderId=order_id)
 
-    # Проверяем есть ли такой рецепт
-    #item_to_create.OrderId
-    #item_to_create.RecipeId
+    _check_order_and_recipe(item_to_create, db)
+
     order: schemas.Orders = crud.OrdersCRUD.read_one(item_to_create.OrderId, db)
     recipe: schemas.Recipe = db.get(models.Recipes, item_to_create.RecipeId)
     if not order:
@@ -24,14 +32,26 @@ def add_item_to_order(item: schemas.OrderItemsCreate, order_id: int, db: Session
     ingredients = db.query(models.RecipeIngredients).filter_by(RecipeId=recipe.Id).all()
 
     for ingredient in ingredients:
+
         foodproduct: schemas.FoodProducts = db.get(models.FoodProducts, ingredient.FoodProductId)
-        
+        stored_food_product = db.query(models.Storage).\
+            filter_by(CafeId=order.CafeId, FoodProductId=ingredient.FoodProductId).first()
+        if not stored_food_product:
+            raise HTTPException(status_code=404, detail=f"Отсутствует документация склада по {foodproduct.Name}")
 
-    #raise HTTPException(status_code=404, detail=f"{} not found")
 
+
+        if ingredient.Number > stored_food_product.Number:
+            raise HTTPException(status_code=422, detail=f"На складе недостаточно {foodproduct.Name}")
+
+        stored_food_product.Number = stored_food_product.Number - ingredient.Number
+
+        a = db.query(models.Storage).filter_by(Id=stored_food_product.Id)#.update(stored_food_product)
+        a.update({"Number": stored_food_product.Number})
+
+        db.commit()
 
 
     ret = crud.OrderItemsCRUD.create(item_to_create, db)
     return ret
-
 
